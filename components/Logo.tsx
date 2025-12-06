b3007988206e184ca31912fcfaada7c1c7fa0fcd
SREEHARI <sreeharis@SREEHARIs-MacBook-Air.local>
2025-12-06 18:22:35 +0530

    your message

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,16 +5,16 @@ interface LogoProps {
   className?: string;
 }
 
-// Converted Google Drive View Link to Direct Image Source
-// File ID: 1fLLtv101NWIUmZYZXm4vU_4CUmxNgEuM
-const LOGO_URL = "https://drive.google.com/uc?export=view&id=1fLLtv101NWIUmZYZXm4vU_4CUmxNgEuM";
+import logoUrl from '../logo.png';
+
+const LOGO_URL = logoUrl;
 
 export const Logo: React.FC<LogoProps> = ({ className = "w-10 h-10" }) => {
   return (
-    <img 
-      src={LOGO_URL} 
-      alt="Crowngate Logo" 
-      className={`${className} object-contain`} 
+    <img
+      src={LOGO_URL}
+      alt="Crowngate Logo"
+      className={`${className} object-contain`}
     />
   );
 };
